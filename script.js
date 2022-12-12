8f6b52c1874db4d1d8cc299286899740fe942b52
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-12 20:14:41 +0000

    added for loop, and edited the functions

--- script.js
+++ script.js
@@ -1,48 +1,56 @@
-console.log('Hello! World!');
-
 // computer choses rock paper or scissors
 
 var options=['rock', 'paper', 'scissors'] 
 
-function getComputerChoice(){
-  const computerChoice= Math.floor(Math.random()*3)
-  return (options[computerChoice]);
-}
-
-let computerSelection = getComputerChoice();
-console.log(computerSelection); //print computer's selection
-
-//player needs to select his choice
-
-let playerSelection= prompt('Please select rock, paper or scissors:').toLowerCase();
-   //to make it non-case-sensitive
-
-    console.log(playerSelection);
-
    //function to play first round 
 function playRound(){
 
+
+    function getComputerChoice(){
+        const computerChoice= Math.floor(Math.random()*3)
+        return (options[computerChoice]);
+      }
+      
+      let computerSelection = getComputerChoice();
+      console.log(computerSelection); //print computer's selection
+      
+      //player needs to select his choice
+      
+      let playerSelection= prompt('Please select rock, paper or scissors:').toLowerCase();
+         //to make it non-case-sensitive
+      
+          console.log(playerSelection);
+
     if (playerSelection == computerSelection){
 
-        alert("it's a tie")
+        console.log("it's a tie")
     }
    else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        alert('You lose! ' +computerSelection +" beats " +playerSelection +"!");
+        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        alert('You won! ' +playerSelection +" beats " +computerSelection +"!");
+        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        alert('You lose! ' +computerSelection +" beats " +playerSelection +"!");
+        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        alert('You won! ' +playerSelection +" beats " +computerSelection +"!");
+        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        alert('You lose! ' +computerSelection +" beats " +playerSelection +"!");
+        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        alert('You won! ' +playerSelection +" beats " +computerSelection +"!");
+        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
+    }
+
+
+}
+
+function game(){
+    for(let i=0; i<5; i++){
+        playRound();
+        
     }
 
 
