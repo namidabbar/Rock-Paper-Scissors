e920979bb461dd69647a2463f66f0ffa4e28b86b
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-20 02:47:54 +0000

    tidied up the code, removed unused functions, and console.logs

--- script.js
+++ script.js
@@ -16,51 +16,37 @@ let playerSelection=document.getElementById('rock').addEventListener("click", ()
  const computerRunningScore=document.getElementById('computerRunningScore');
  const playerRunningScore=document.getElementById('playerRunningScore');
 
-   //function to play first round 
+//function to play a round 
 function playRound(playerSelection){
 
-
+    // computer choses rock paper or scissors
     function getComputerChoice(){
         const computerChoice= Math.floor(Math.random()*3)
         return (options[computerChoice]);
-      }
+    }
       
-      let computerSelection = getComputerChoice();
-      //console.log(computerSelection); //this printed computer's selection but could be used to cheat hence commented out.
-      
-      //player needs to select his choice, but we will use buttons now
-      
-      //let playerSelection= prompt('Please select rock, paper or scissors:').toLowerCase();
-         //to make it non-case-sensitive
-      
-        console.log(playerSelection);
+    let computerSelection = getComputerChoice();
 
     if (playerSelection == computerSelection){
-
-        console.log("it's a tie");
         results.textContent="It's a tie, try again!"
         return;
-
     }
    else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
         pointsComputer.push(1);
-    results.textContent="You lose, " +computerSelection +" beats " +playerSelection +"!";
-    computerRunningScore.textContent="Computer's score: "+pointsComputer.length;
+        results.textContent="You lose, " +computerSelection +" beats " +playerSelection +"!";
+        computerRunningScore.textContent="Computer's score: "+pointsComputer.length;
     winner();
         return;
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         pointsPlayer.push(1);
-        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
-        results.textContent= 'You won! ' +playerSelection +" beats " +computerSelection +"!";
+        results.textContent= 'You won, ' +playerSelection +" beats " +computerSelection +"!";
         playerRunningScore.textContent="Players's score: "+pointsPlayer.length;
         winner();
         return;
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors'){
         pointsComputer.push(1);
-        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
         results.textContent="You lose, " +computerSelection +" beats " +playerSelection +"!";
         computerRunningScore.textContent="Computer's score: "+pointsComputer.length;
         winner();
@@ -68,15 +54,13 @@ function playRound(playerSelection){
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock'){
         pointsPlayer.push(1);
-        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
-        results.textContent= 'You won! ' +playerSelection +" beats " +computerSelection +"!";
+        results.textContent= 'You won, ' +playerSelection +" beats " +computerSelection +"!";
         playerRunningScore.textContent="Players's score: "+pointsPlayer.length;
         winner();
         return;
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock'){
         pointsComputer.push(1);
-        console.log('You lose! ' +computerSelection +" beats " +playerSelection +"!");
         results.textContent="You lose, " +computerSelection +" beats " +playerSelection +"!";
         computerRunningScore.textContent="Computer's score: "+pointsComputer.length;
         winner();
@@ -84,29 +68,12 @@ function playRound(playerSelection){
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper'){
         pointsPlayer.push(1);
-        console.log('You won! ' +playerSelection +" beats " +computerSelection +"!");
-        results.textContent= 'You won! ' +playerSelection +" beats " +computerSelection +"!" ;
+        results.textContent= 'You won, ' +playerSelection +" beats " +computerSelection +"!" ;
         playerRunningScore.textContent="Players's score: "+pointsPlayer.length;
         winner();
         return;
     }
-
 }
-//The following game code will not be used for the UI game, only for the game on the console
-/*
-function game(){
-    for(let i=0; i<5; i++){
-        playRound();
-        console.log(points)
-        
-    }
-let score = points[0]+points[1]+points[2]+points[3]+points[4];
-if (score>=3){
-    console.log('Well done, you won! Your score is ' + score);
-}
-else {
-console.log("Oops! The Computer won! Your score was "+score);
-}}*/
 
 function winner(){
     if (pointsComputer.length===5 || pointsPlayer.length===5){
@@ -121,4 +88,3 @@ function winner(){
         return;
     }
 }
-
